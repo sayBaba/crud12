066ea8b80766f5ad18b82416867ccb1a28469e6d
answer123@foxmai.com <answer123@foxmai.com>
2020-06-17 01:28:59 +0800

    shiro

--- src/main/java/com/hzit/crud/controller/AdminController.java
+++ src/main/java/com/hzit/crud/controller/AdminController.java
@@ -5,6 +5,12 @@ import com.hzit.crud.req.LoginReq;
 import com.hzit.crud.resp.MenuResp;
 import com.hzit.crud.resp.RespVo;
 import com.hzit.crud.service.impl.IMenuService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +18,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sun.security.provider.MD5;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
@@ -42,22 +50,47 @@ public class AdminController {
     /**
      * 用户登录
      */
+    @ResponseBody
     @RequestMapping("/login")
-    public RespVo login(@RequestBody LoginReq loginReq,HttpServletRequest request){
-        logger.info("用户登录请求参数loginReq:{}",loginReq);
+    public RespVo login(@RequestParam("username")String userName,
+                        @RequestParam("password")String password,
+                        @RequestParam("vcode") String vcode,
+                        HttpServletRequest request){
+        logger.info("用户登录请求参数vcode:{}",vcode);
 
 
-        if (StringUtils.isEmpty(loginReq.getVcode())){
+        if (StringUtils.isEmpty(vcode)){
             logger.info("验证码为空");
             return RespVo.getFail("验证码为空");
         }
 
        String sessionCode = (String)request.getSession().getAttribute("kaptcha");
 
-        if(!loginReq.getVcode().equals(sessionCode)){
+        if(!vcode.equals(sessionCode)){
             return RespVo.getFail("输入验证码不正确");
         }
 
+        Subject subject = SecurityUtils.getSubject();
+//        password = MD5.makeMD5(password);
+
+        if(!subject.isAuthenticated()){
+            UsernamePasswordToken token = new UsernamePasswordToken(userName, "e10adc3949ba59abbe56e057f20f883e");
+            try {
+                subject.login(token);
+                // 剔除其他此账号在其它地方登录
+
+            } catch (UnknownAccountException ex1) {
+
+                ex1.printStackTrace();
+            } catch (IncorrectCredentialsException ex2) {
+                ex2.printStackTrace();
+
+            }catch (AuthenticationException ex3) {
+                ex3.printStackTrace();
+
+            }
+
+        }
 
         //shiro 登录逻辑 TODO
 
@@ -97,6 +130,11 @@ public class AdminController {
     @RequestMapping("/main")
     public String loadMain(){
         return "main";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+        return "redirect:/index.jsp";
     }
 
 }
